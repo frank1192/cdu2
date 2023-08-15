bc8b300bd7fb215444689277c2abdd28c40adfe7
unknown <franklin.aguirre@correounivalle.edu.co>
2023-08-14 20:41:15 -0500

    un hola mundo

--- ingresos/views.py
+++ ingresos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def index(request):
+    template_name="index.html"
+    return render(request, template_name)
